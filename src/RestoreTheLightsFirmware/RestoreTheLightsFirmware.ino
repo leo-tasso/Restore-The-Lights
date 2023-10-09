2481f04f25b7ca34aa3cc4134e5e22b77d3aa8ec
Leonardo Tassinari <leonardo.tassinari6@studio.unibo.it>
2023-10-09 14:23:10 +0200

    create base functions

--- src/RestoreTheLightsFirmware/RestoreTheLightsFirmware.ino
+++ src/RestoreTheLightsFirmware/RestoreTheLightsFirmware.ino
@@ -1,12 +1,15 @@
 #include <avr/sleep.h>
 #include "config.h"
 #include <EnableInterrupt.h>
-
+#include <stdlib.h>
 
 short int brightness = 0;
 short int fadeAmount = 5;
-long entred_state_time;
-long current_time_in_state;
+unsigned long entred_state_time;
+unsigned long T1 = 0;
+int ledsOn =COLUMNS;
+game_state activeGameState = WAIT_START;
+int sequence[COLUMNS] = {1,2,4,8};
 void waitStart();
 
 void game(float T2, float T3);
@@ -15,6 +18,7 @@ void deepSleep();
 
 void setup(){
     //Setup pins
+    srand(millis()%100);
     pinMode(pot,INPUT);
     pinMode(LS, OUTPUT);
     for(int i=0; i<COLUMNS; i++){
@@ -56,4 +60,53 @@ void waitStart() {
  void startGame(){
   disableInterrupt(pinB[1]);
   game();
+}
+
+void waitTime(){
+    if(T1==0){
+        T1=rand()% MAX_WAIT_TIME;
+        generateSequence();
+        //TODO turn on all leds
+        ledsOn=COLUMNS;
+    }
+    if(millis()-entred_state_time>=T1){
+        T1=0;
+        changeGameMode(DISPLAY_SEQUENCE);
+    }
+}
+
+void displaySequence(){
+    if(ledsOn==0){
+        changeGameMode(USER_GAMEPLAY);
+    }
+    else if(entred_state_time-millis()<T2/ledsOn){
+        //TODO turn off first led
+        ledsOn--;
+    }
+}
+
+void userGameplay(){
+    if(entred_state_time-millis()>T3 || (pressedButtons!=sequence[ledsOn]&&pressedButtons!=0)){
+        gameOver();
+    }else if(ledsOn<COLUMNS){
+        //TODO turn on first led sequence
+        ledsOn++;
+    }else{
+        //TODO win
+        changeGameMode(WAIT_START);
+    }
+    void changeGameMode(game_state state){
+        activeGameState = state;
+        entred_state_time = millis();
+    }
+
+    void generateSequence(){
+        for (int i = COLUMNS - 1; i > 0; i--) {
+            int j = random(0, i + 1);
+            // Swap array[i] and array[j]
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
 }
--- src/RestoreTheLightsFirmware/config.h
+++ src/RestoreTheLightsFirmware/config.h
@@ -1,11 +1,10 @@
 #ifndef CONFIG
 #define CONFIG
-
 #define pot A0      //Pin of potentiometer
 #define LS 13       //Pin of status LED, must be PWM pin
 #define COLUMNS 4   //The number of pairs led-button
-
-const enum game_state = {WAIT_START, DISPLAY_SEQUENCE, USER_GAMEPLAY, SLEEP}
+#define MAX_WAIT_TIME 1000
+const enum game_state = {WAIT_START,WAIT_START_TIME, DISPLAY_SEQUENCE, USER_GAMEPLAY, SLEEP}
 const unsigned short pinL[COLUMNS] = {6,7,8,9} ;
 const unsigned short pinB[COLUMNS] = {2,3,4,5} ;
 #endif
